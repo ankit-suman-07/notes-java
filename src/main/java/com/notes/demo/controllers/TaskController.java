f6a526390cdd5e5819d3aa3c43dbf1b722544c51
Ankit Suman <ankitsuman07@gmail.com>
2025-01-08 23:40:49 +0530

    Create DTO for tasks

--- src/main/java/com/notes/demo/controllers/TaskController.java
+++ src/main/java/com/notes/demo/controllers/TaskController.java
@@ -2,9 +2,9 @@ package com.notes.demo.controllers;
 
 import com.notes.demo.entities.TaskEntity;
 import com.notes.demo.service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +20,29 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping("/get-all-tasks")
+    @GetMapping("/get-tasks")
     public ResponseEntity<List<TaskEntity>> getTasks() {
         var tasks = taskService.getTasks();
         return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/get-all-tasks")
+    public List<TaskEntity> getTasksList() {
+        var tasks = taskService.getTasks();
+        return tasks;
+    }
+
+    @GetMapping("/{id")
+    public ResponseEntity<TaskEntity> getTaskById(int id) {
+        var task = taskService.getTaskById(id);
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
+    }
+
+    @PostMapping("/{id")
+    public ResponseEntity<TaskEntity> addTask(){
+        taskService.addTask(String title, String description, String deadline);
     }
 }
