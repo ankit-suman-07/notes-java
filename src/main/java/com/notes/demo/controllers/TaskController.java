b0b47693355c6b0868407efb8167f34796239146
Ankit Suman <ankitsuman07@gmail.com>
2025-01-09 00:22:30 +0530

    Include Ex eption handling

--- src/main/java/com/notes/demo/controllers/TaskController.java
+++ src/main/java/com/notes/demo/controllers/TaskController.java
@@ -1,11 +1,13 @@
 package com.notes.demo.controllers;
 
 import com.notes.demo.dto.CreateTaskDTO;
+import com.notes.demo.dto.ErrorResponseDTO;
 import com.notes.demo.entities.TaskEntity;
 import com.notes.demo.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -40,8 +42,17 @@ public class TaskController {
     }
 
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO createTaskDTO){
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO createTaskDTO) throws ParseException {
         var task = taskService.addTask(createTaskDTO.getTitle(), createTaskDTO.getDescription(), createTaskDTO.getDeadline());
         return ResponseEntity.ok(task);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e) {
+        if(e instanceof ParseException) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid Date Formatter!!!"));
+        }
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Internal Server"));
     }
 }
--- src/main/java/com/notes/demo/service/TaskService.java
+++ src/main/java/com/notes/demo/service/TaskService.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -13,13 +14,14 @@ public class TaskService {
     @Getter
     private ArrayList<TaskEntity> tasks = new ArrayList<>();
     private int taskId = 1;
+    private SimpleDateFormat deadlineFormat = new SimpleDateFormat("YYYY-MM-DD");
 
-    public TaskEntity addTask(String title, String description, String deadline) {
+    public TaskEntity addTask(String title, String description, String deadline) throws ParseException {
         TaskEntity taskEntity = new TaskEntity();
         taskEntity.setId(taskId);
         taskEntity.setTitle(title);
         taskEntity.setDescription(description);
-        //taskEntity.setDeadline(new Date(deadline)); // TODO: validate date format yyyy-mm-dd
+        taskEntity.setDeadline(deadlineFormat.parse(deadline)); // TODO: validate date format yyyy-mm-dd
         taskEntity.setCompleted(false);
         tasks.add(taskEntity);
         taskId++;
